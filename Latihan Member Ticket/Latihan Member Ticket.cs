13b8edde18dbb2a689d15127ebdeb363f4ec78d6
Vinda29 <vindahandayani29@students.amikom.ac.id>
2023-04-06 07:43:05 +0700

    Add project files.

---
+++ Latihan Member Ticket/Latihan Member Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Member_Ticket
+{
+    class PlanetTicket
+    {
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public int Cost { get; set; }
+        public string Currency { get; set; }
+        public void PrintSummary()
+        {
+            Console.WriteLine("Origin: {0}", Origin);
+            Console.WriteLine("Destinaton: {0}", Destination);
+            Console.WriteLine("Cost: {0}", Cost, Currency);
+            if(Currency == "IDR")
+            {
+                Cost = Cost / 15000;
+                Console.WriteLine("Convert to Rupiah: {0}", Cost);
+            }else if (Currency == "USD")
+            {
+                Cost = Cost * 15000;
+                Console.WriteLine("Convert to Dollar: {0}", Cost);
+            }
+        }
+    }
+}
